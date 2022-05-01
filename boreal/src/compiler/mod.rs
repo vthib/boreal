112f06ef23c35e684067085497832a3810799f3b
Vincent Thiberville <vthib@pm.me>
2022-05-01 22:24:38 +0200

    refacto: move rule compiler stuff in own module

--- boreal/src/compiler/mod.rs
+++ boreal/src/compiler/mod.rs
@@ -1,6 +1,5 @@
 //! Compilation of a parsed expression into an optimized one.
 use std::collections::HashMap;
-use std::ops::Range;
 
 use codespan_reporting::diagnostic::Diagnostic;
 use codespan_reporting::files::SimpleFile;
@@ -17,6 +16,8 @@ mod variable;
 pub use variable::*;
 mod module;
 pub use module::*;
+mod rule;
+pub use rule::*;
 
 use crate::Scanner;
 
@@ -30,25 +31,6 @@ pub struct Compiler {
     ///
     /// These are modules that can be imported and used in the namespaces.
     available_modules: HashMap<String, Module>,
-}
-
-/// A compiled scanning rule.
-#[derive(Debug)]
-pub struct Rule {
-    /// Name of the rule.
-    pub name: String,
-
-    /// Tags associated with the rule.
-    pub tags: Vec<String>,
-
-    /// Metadata associated with the rule.
-    pub metadatas: Vec<parser::Metadata>,
-
-    /// Variable associated with the rule
-    pub(crate) variables: Vec<Variable>,
-
-    /// Condition of the rule.
-    pub(crate) condition: Expression,
 }
 
 impl Compiler {
@@ -106,102 +88,6 @@ impl Compiler {
     pub fn into_scanner(self) -> Scanner {
         Scanner::new(self.rules)
     }
-}
-
-/// Object used to compile a rule.
-struct RuleCompiler<'a> {
-    /// Symbols available to use in the rule.
-    ///
-    /// Those symbols come from two sources:
-    /// - imported modules in the file
-    /// - rules included, or rules declared earlier in the file.
-    symbols: &'a HashMap<String, &'a Module>,
-
-    /// Map of variable name to index in the compiled rule variables vec.
-    ///
-    /// This only stores named variables. Anonymous ones are still stored
-    /// (and thus have an index), but cannot be referred by name.
-    // TODO: hashset of used variables per index, to indicate which ones
-    // are unused.
-    variables_map: HashMap<String, usize>,
-}
-
-impl<'a> RuleCompiler<'a> {
-    fn new(
-        rule: &parser::Rule,
-        symbols: &'a HashMap<String, &'a Module>,
-    ) -> Result<Self, CompilationError> {
-        let mut variables_map = HashMap::new();
-        for (idx, var) in rule.variables.iter().enumerate() {
-            if var.name.is_empty() {
-                continue;
-            }
-            if variables_map.insert(var.name.clone(), idx).is_some() {
-                return Err(CompilationError::DuplicatedVariable(var.name.clone()));
-            }
-        }
-
-        Ok(Self {
-            symbols,
-            variables_map,
-        })
-    }
-
-    /// Find a variable used in a rule by name.
-    ///
-    /// The provided span is the one of the expression using the variable, and is
-    /// used for the error if the find fails.
-    ///
-    /// This function allows anonymous variables. To only allow named variable, use
-    /// [`self.find_named_variable`] instead.
-    fn find_variable(
-        &self,
-        name: &str,
-        span: &Range<usize>,
-    ) -> Result<VariableIndex, CompilationError> {
-        if name.is_empty() {
-            Ok(VariableIndex(None))
-        } else {
-            Ok(VariableIndex(Some(self.find_named_variable(name, span)?)))
-        }
-    }
-
-    /// Find a variable used in a rule by name, without accepting anonymous variables.
-    fn find_named_variable(
-        &self,
-        name: &str,
-        span: &Range<usize>,
-    ) -> Result<usize, CompilationError> {
-        match self.variables_map.get(name) {
-            Some(index) => Ok(*index),
-            None => Err(CompilationError::UnknownVariable {
-                variable_name: name.to_owned(),
-                span: span.clone(),
-            }),
-        }
-    }
-}
-
-fn compile_rule<'a>(
-    rule: parser::Rule,
-    symbols: &'a HashMap<String, &'a Module>,
-) -> Result<Rule, CompilationError> {
-    let compiler = RuleCompiler::new(&rule, symbols)?;
-    let condition = compile_expression(&compiler, rule.condition)?;
-
-    Ok(Rule {
-        name: rule.name,
-        tags: rule.tags,
-        metadatas: rule.metadatas,
-        variables: rule
-            .variables
-            .into_iter()
-            .map(compile_variable)
-            .collect::<Result<Vec<_>, _>>()?,
-        condition: condition.expr,
-    })
-
-    // TODO: check for unused variables
 }
 
 #[derive(Debug)]
---
+++ boreal/src/compiler/rule.rs
@@ -0,0 +1,124 @@
+use std::collections::HashMap;
+use std::ops::Range;
+
+use boreal_parser as parser;
+
+use super::{
+    compile_expression, compile_variable, CompilationError, Expression, Module, Variable,
+    VariableIndex,
+};
+
+/// A compiled scanning rule.
+#[derive(Debug)]
+pub struct Rule {
+    /// Name of the rule.
+    pub name: String,
+
+    /// Tags associated with the rule.
+    pub tags: Vec<String>,
+
+    /// Metadata associated with the rule.
+    pub metadatas: Vec<parser::Metadata>,
+
+    /// Variable associated with the rule
+    pub(crate) variables: Vec<Variable>,
+
+    /// Condition of the rule.
+    pub(crate) condition: Expression,
+}
+
+/// Object used to compile a rule.
+pub(super) struct RuleCompiler<'a> {
+    /// Symbols available to use in the rule.
+    ///
+    /// Those symbols come from two sources:
+    /// - imported modules in the file
+    /// - rules included, or rules declared earlier in the file.
+    pub symbols: &'a HashMap<String, &'a Module>,
+
+    /// Map of variable name to index in the compiled rule variables vec.
+    ///
+    /// This only stores named variables. Anonymous ones are still stored
+    /// (and thus have an index), but cannot be referred by name.
+    // TODO: hashset of used variables per index, to indicate which ones
+    // are unused.
+    pub variables_map: HashMap<String, usize>,
+}
+
+impl<'a> RuleCompiler<'a> {
+    pub(super) fn new(
+        rule: &parser::Rule,
+        symbols: &'a HashMap<String, &'a Module>,
+    ) -> Result<Self, CompilationError> {
+        let mut variables_map = HashMap::new();
+        for (idx, var) in rule.variables.iter().enumerate() {
+            if var.name.is_empty() {
+                continue;
+            }
+            if variables_map.insert(var.name.clone(), idx).is_some() {
+                return Err(CompilationError::DuplicatedVariable(var.name.clone()));
+            }
+        }
+
+        Ok(Self {
+            symbols,
+            variables_map,
+        })
+    }
+
+    /// Find a variable used in a rule by name.
+    ///
+    /// The provided span is the one of the expression using the variable, and is
+    /// used for the error if the find fails.
+    ///
+    /// This function allows anonymous variables. To only allow named variable, use
+    /// [`self.find_named_variable`] instead.
+    pub(super) fn find_variable(
+        &self,
+        name: &str,
+        span: &Range<usize>,
+    ) -> Result<VariableIndex, CompilationError> {
+        if name.is_empty() {
+            Ok(VariableIndex(None))
+        } else {
+            Ok(VariableIndex(Some(self.find_named_variable(name, span)?)))
+        }
+    }
+
+    /// Find a variable used in a rule by name, without accepting anonymous variables.
+    pub(super) fn find_named_variable(
+        &self,
+        name: &str,
+        span: &Range<usize>,
+    ) -> Result<usize, CompilationError> {
+        match self.variables_map.get(name) {
+            Some(index) => Ok(*index),
+            None => Err(CompilationError::UnknownVariable {
+                variable_name: name.to_owned(),
+                span: span.clone(),
+            }),
+        }
+    }
+}
+
+pub(super) fn compile_rule<'a>(
+    rule: parser::Rule,
+    symbols: &'a HashMap<String, &'a Module>,
+) -> Result<Rule, CompilationError> {
+    let compiler = RuleCompiler::new(&rule, symbols)?;
+    let condition = compile_expression(&compiler, rule.condition)?;
+
+    Ok(Rule {
+        name: rule.name,
+        tags: rule.tags,
+        metadatas: rule.metadatas,
+        variables: rule
+            .variables
+            .into_iter()
+            .map(compile_variable)
+            .collect::<Result<Vec<_>, _>>()?,
+        condition: condition.expr,
+    })
+
+    // TODO: check for unused variables
+}
