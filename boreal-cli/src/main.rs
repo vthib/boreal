056f83de18e3aea4aa01e481fa7596f86a1bfeee
Vincent Thiberville <vthib@pm.me>
2022-07-02 22:01:56 +0200

    feat: use clap in boreal-cli to handle arguments

--- boreal-cli/src/main.rs
+++ boreal-cli/src/main.rs
@@ -2,6 +2,7 @@ use std::path::PathBuf;
 use std::process::exit;
 
 use boreal::Compiler;
+use clap::Parser;
 use codespan_reporting::{
     files::SimpleFile,
     term::{
@@ -10,32 +11,46 @@ use codespan_reporting::{
     },
 };
 
+#[derive(Parser, Debug)]
+#[clap(version, about)]
+struct Args {
+    /// Path to a yara file containing rules.
+    #[clap(value_parser)]
+    rules_file: PathBuf,
+
+    /// Input to scan.
+    #[clap(value_parser)]
+    file: PathBuf,
+}
+
 fn main() -> Result<(), std::io::Error> {
-    // TODO: make a proper binary with proper parameters
-    let yara_filepath = std::env::args().nth(1).unwrap_or_else(|| {
-        eprintln!("give a path to a yara file as argument to this binary");
-        exit(1);
-    });
+    let args = Args::parse();
 
-    let path = PathBuf::from(&yara_filepath);
-    let contents = std::fs::read_to_string(&path)?;
+    let scanner = {
+        let rules_contents = std::fs::read_to_string(&args.rules_file)?;
 
-    let mut compiler = Compiler::new();
-    match compiler.add_rules_str(&contents) {
-        Err(err) => {
+        let mut compiler = Compiler::new();
+        if let Err(err) = compiler.add_rules_str(&rules_contents) {
             let writer = StandardStream::stderr(ColorChoice::Always);
             let config = codespan_reporting::term::Config::default();
 
-            let files = SimpleFile::new(yara_filepath, &contents);
+            let path = args.rules_file.display().to_string();
+            let files = SimpleFile::new(&path, &rules_contents);
             let diag = err.to_diagnostic();
             if let Err(e) = term::emit(&mut writer.lock(), &config, &files, &diag) {
                 eprintln!("cannot emit diagnostics: {}", e);
             }
             exit(2);
         }
-        Ok(_) => {
-            println!("successfully added rules from {}", path.display());
-        }
+
+        compiler.into_scanner()
+    };
+
+    let file_contents = std::fs::read(&args.file)?;
+    let res = scanner.scan_mem(&file_contents);
+    for rule in res.matched_rules {
+        println!("{} {}", &rule.name, args.file.display());
     }
+
     Ok(())
 }
