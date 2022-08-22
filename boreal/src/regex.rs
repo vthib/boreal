b931aaf65857ab6fca119e2a10fd7bff4f491baa
Vincent Thiberville <vthib@pm.me>
2022-08-22 22:13:42 +0200

    feat: remove useless escapes in regex
    Yara regex allow using escapes on characters that do not need to be
    escaped, without causing errors. This needs to be handled, otherwise
    this causes parsing errors when the regex is given to the rust crate.

--- boreal/src/regex.rs
+++ boreal/src/regex.rs
@@ -1,3 +1,5 @@
+use std::fmt::Write;
+
 use regex::bytes::RegexBuilder;
 
 /// Regex following the YARA format.
@@ -47,9 +49,61 @@ impl Regex {
 /// - While the regex might be a string, it gets matched as a byte string by Yara. This leads
 ///   to some confusing behavior, for example `/é+/` will match `\xC3\xA9` (é in unicode) but will
 ///   match `\xC3\xA9\xA9` and not match `\xC3\xA9\xC3\xA9`.
+/// - Yara regexes allow any backslash: if the character is not special, it will act as if the
+///   backslash was not present.
 pub(crate) fn normalize_regex(expr: &str) -> String {
-    use std::fmt::Write;
+    let expr = decompose_unicode_bytes(expr);
+    remove_unneeded_escapes(&expr)
+}
 
+fn remove_unneeded_escapes(expr: &str) -> String {
+    let mut res = String::with_capacity(expr.len());
+    let mut prev_is_escape = false;
+
+    for b in expr.as_bytes() {
+        match (prev_is_escape, *b) {
+            (false, b'\\') => prev_is_escape = true,
+            (false, b) => res.push(char::from(b)),
+            (true, b'\\') => {
+                res.push_str(r"\\");
+                prev_is_escape = false;
+            }
+            (true, c) => {
+                // unicode values have already been decomposed. It is thus safe to iterate on
+                // bytes, and cast as chars.
+                let c = char::from(c);
+
+                // If the byte is escapable, keep the escape and the byte.
+                // Otherwise, this means the escape char is useless
+                if byte_is_escapable(c) {
+                    res.push('\\');
+                }
+                res.push(c);
+                prev_is_escape = false;
+            }
+        }
+    }
+    if prev_is_escape {
+        res.push('\\');
+    }
+
+    res
+}
+
+fn byte_is_escapable(c: char) -> bool {
+    // These are the escapable bytes for a YARA regex. These are not all the accepted escapable
+    // bytes for a rust regex, meaning those are not available in yara rules.
+    match c {
+        // TODO: Technically, 'b' and 'B' are not accepted in a char range. So this code is invalid
+        // for this, and writing [\b] in a yara rule will not work...
+        'x' | 'n' | 't' | 'r' | 'f' | 'a' => true,
+        'w' | 'W' | 's' | 'S' | 'd' | 'D' | 'b' | 'B' => true,
+        c if regex_syntax::is_meta_character(c) => true,
+        _ => false,
+    }
+}
+
+fn decompose_unicode_bytes(expr: &str) -> String {
     let mut res = String::with_capacity(expr.len());
 
     // Decompose non ascii chars into their utf-8 encoding, as the yara regex
--- boreal/tests/it/regex.rs
+++ boreal/tests/it/regex.rs
@@ -1,4 +1,17 @@
-use crate::utils::Checker;
+use crate::utils::{check, Checker};
+
+fn build_rule(var: &str) -> String {
+    format!(
+        r#"
+rule a {{
+    strings:
+        $ = {}
+    condition:
+        all of them
+}}"#,
+        var
+    )
+}
 
 #[test]
 fn test_regex_unicode_handling() {
@@ -22,4 +35,64 @@ rule a {
     checker.check(b"<\xC3\xA9\xA9\xA9\xA9>", true);
     checker.check(b"<\xC3\xA9\xA9\xA9\xA9", false);
     checker.check(b"\xC3\xA9\xA9\xA9\xA9>", false);
+}
+
+#[test]
+fn test_regex_unneeded_escapes() {
+    // Escaping for specific bytes
+    check(
+        &build_rule(r"/<\a\f\r\t\n\xE9>/"),
+        b"<\x07\x0C\r\t\n\xE9>",
+        true,
+    );
+
+    // Escaping for specific regex behavior
+    let checker = Checker::new(&build_rule(r"/<\w\W>/"));
+    checker.check(b"<ab>", false);
+    checker.check(b"<a/>", true);
+    checker.check(b"<_]>", true);
+    checker.check(b"<8]>", true);
+    checker.check(b"<88>", false);
+    checker.check(b"<[]>", false);
+    let checker = Checker::new(&build_rule(r"/<\d\D>/"));
+    checker.check(b"<ab>", false);
+    checker.check(b"<7b>", true);
+    checker.check(b"<77>", false);
+    let checker = Checker::new(&build_rule(r"/<\s\S>/"));
+    checker.check(b"<ab>", false);
+    checker.check(b"< b>", true);
+    checker.check(b"<\tb>", true);
+    checker.check(b"<\t >", false);
+    let checker = Checker::new(&build_rule(r"/\bab\B/"));
+    checker.check(b"ab", false);
+    checker.check(b"<ab>", false);
+    checker.check(b"abc", true);
+    checker.check(b"<abc", true);
+    checker.check(b"zabc", false);
+    checker.check(b"zab", false);
+
+    // Escaping for special regex characters
+    check(&build_rule(r"/<\.\[\]\+\*\?>/"), b"<.[]+*?>", true);
+    check(&build_rule(r"/\\/"), b"\\", true);
+
+    // Escaping the '/' character is handled during parsing.
+    check(&build_rule(r"/<\/>/"), b"</>", true);
+
+    // Escaping on other stuff will discard the escape
+    check(&build_rule(r#"/\<\z\/\!\ \"\'\>/"#), br#"<z/! "'>"#, true);
+
+    // Test from libyara: makes no sense, but works because of useless escapes being removed
+    // This accepts 0, x, 5, then A-Z [ \\ and ]
+    // This is *not* \x5A to \x5D
+    let checker = Checker::new(&build_rule(r"/[\0x5A-\x5D]/"));
+    checker.check(b"0", true);
+    checker.check(b"1", false);
+    checker.check(b"x", true);
+    checker.check(b"5", true);
+    checker.check(b"A", true);
+    checker.check(b"Z", true);
+    checker.check(b"[", true);
+    checker.check(b"\\", true);
+    checker.check(b"]", true);
+    checker.check(b"^", false);
 }
