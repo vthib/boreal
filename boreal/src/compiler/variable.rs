5d029821915ef99557dd92df1f7b83e8733d4224
Vincent Thiberville <vthib@pm.me>
2022-09-11 16:29:29 +0200

    refacto: move hex string compilation to new module

--- boreal/src/compiler/variable.rs
+++ boreal/src/compiler/variable.rs
@@ -1,17 +1,13 @@
 use ::regex::bytes::{Regex, RegexBuilder};
 use aho_corasick::{AhoCorasick, AhoCorasickBuilder};
 
-use boreal_parser::regex::{
-    BracketedClass, BracketedClassItem, ClassKind, Node, RepetitionKind, RepetitionRange,
-};
-use boreal_parser::{HexMask, HexToken, VariableFlags, VariableModifiers};
 use boreal_parser::{VariableDeclaration, VariableDeclarationValue};
-
-use crate::regex::add_ast_to_string;
+use boreal_parser::{VariableFlags, VariableModifiers};
 
 use super::base64::encode_base64;
 use super::CompilationError;
 
+mod hex_string;
 mod regex;
 
 /// A compiled variable used in a rule.
@@ -111,18 +107,7 @@ pub(crate) fn compile_variable(decl: VariableDeclaration) -> Result<Variable, Co
             flags.remove(VariableFlags::FULLWORD);
             flags.remove(VariableFlags::WIDE);
 
-            if let Some(literals) = hex_string_to_literals(&hex_string) {
-                VariableExpr::Literals {
-                    literals: vec![literals],
-                    case_insensitive: false,
-                }
-            } else {
-                let ast = hex_string_to_ast(hex_string);
-                let mut expr = String::new();
-                expr.push_str("(?s)");
-                add_ast_to_string(ast, &mut expr);
-                VariableExpr::Regex(expr)
-            }
+            hex_string::compile_hex_string(hex_string)
         }
     };
 
@@ -237,62 +222,6 @@ fn string_to_wide(s: &[u8]) -> Vec<u8> {
     res
 }
 
-fn hex_string_to_literals(hex_string: &[HexToken]) -> Option<Vec<u8>> {
-    let mut literals = Vec::new();
-
-    for token in hex_string {
-        match token {
-            HexToken::Byte(b) => literals.push(*b),
-            HexToken::Jump(_) => return None,
-            // TODO: both of those could be handled
-            HexToken::MaskedByte(_, _) => return None,
-            HexToken::Alternatives(_) => return None,
-        }
-    }
-
-    Some(literals)
-}
-
-fn hex_string_to_ast(hex_string: Vec<HexToken>) -> Node {
-    Node::Concat(hex_string.into_iter().map(hex_token_to_ast).collect())
-}
-
-fn hex_token_to_ast(token: HexToken) -> Node {
-    match token {
-        HexToken::Byte(b) => Node::Literal(b),
-        HexToken::MaskedByte(b, mask) => match mask {
-            HexMask::Left => Node::Class(ClassKind::Bracketed(BracketedClass {
-                items: (0..=0xF)
-                    .map(|i| BracketedClassItem::Literal((i << 4) + b))
-                    .collect(),
-                negated: false,
-            })),
-            HexMask::Right => {
-                let b = b << 4;
-                Node::Class(ClassKind::Bracketed(BracketedClass {
-                    items: vec![BracketedClassItem::Range(b, b + 0x0F)],
-                    negated: false,
-                }))
-            }
-            HexMask::All => Node::Dot,
-        },
-        HexToken::Jump(jump) => {
-            let kind = match (jump.from, jump.to) {
-                (from, None) => RepetitionKind::Range(RepetitionRange::AtLeast(from)),
-                (from, Some(to)) => RepetitionKind::Range(RepetitionRange::Bounded(from, to)),
-            };
-            Node::Repetition {
-                node: Box::new(Node::Dot),
-                kind,
-                greedy: false,
-            }
-        }
-        HexToken::Alternatives(elems) => Node::Group(Box::new(Node::Alternation(
-            elems.into_iter().map(hex_string_to_ast).collect(),
-        ))),
-    }
-}
-
 /// Error during the compilation of a variable.
 #[derive(Debug)]
 pub enum VariableCompilationError {
@@ -313,48 +242,5 @@ impl std::fmt::Display for VariableCompilationError {
             // This should not happen. Please report it upstream if it does.
             Self::WidenError => write!(f, "unable to apply the wide modifier to the regex"),
         }
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use boreal_parser::parse_str;
-
-    #[track_caller]
-    fn test(hex_string: &str, expected_regex: &str) {
-        let rule_str = format!("rule a {{ strings: $a = {} condition: $a }}", hex_string);
-        let mut file = parse_str(&rule_str).unwrap();
-        let mut rule = file
-            .components
-            .pop()
-            .map(|v| match v {
-                boreal_parser::YaraFileComponent::Rule(v) => v,
-                _ => panic!(),
-            })
-            .unwrap();
-        let var = rule.variables.pop().unwrap();
-        let hex_string = match var.value {
-            VariableDeclarationValue::HexString(s) => s,
-            _ => panic!(),
-        };
-
-        let ast = hex_string_to_ast(hex_string);
-        let mut regex = String::new();
-        add_ast_to_string(ast, &mut regex);
-        assert_eq!(regex, expected_regex);
-    }
-
-    #[test]
-    fn test_hex_string_to_regex() {
-        test(
-            "{ AB ?D 01 }",
-            r"\xab[\x0d\x1d\x2d=M\x5dm\x7d\x8d\x9d\xad\xbd\xcd\xdd\xed\xfd]\x01",
-        );
-        test("{ C7 [-] ?? }", r"\xc7.{0,}?.");
-        test(
-            "{ C7 [3-] 5? 03 [-6] C7 ( FF 15 | E8 ) [4] 6A ( FF D? | E8 [2-4] ??) }",
-            r"\xc7.{3,}?[P-_]\x03.{0,6}?\xc7(\xff\x15|\xe8).{4,4}?j(\xff[\xd0-\xdf]|\xe8.{2,4}?.)",
-        );
     }
 }
---
+++ boreal/src/compiler/variable/hex_string.rs
@@ -0,0 +1,122 @@
+use boreal_parser::regex::{
+    BracketedClass, BracketedClassItem, ClassKind, Node, RepetitionKind, RepetitionRange,
+};
+use boreal_parser::{HexMask, HexToken};
+
+use crate::regex::add_ast_to_string;
+
+use super::VariableExpr;
+
+pub(super) fn compile_hex_string(hex_string: Vec<HexToken>) -> VariableExpr {
+    if let Some(literals) = hex_string_to_literals(&hex_string) {
+        VariableExpr::Literals {
+            literals: vec![literals],
+            case_insensitive: false,
+        }
+    } else {
+        let ast = hex_string_to_ast(hex_string);
+        let mut expr = String::new();
+        expr.push_str("(?s)");
+        add_ast_to_string(ast, &mut expr);
+        VariableExpr::Regex(expr)
+    }
+}
+
+fn hex_string_to_literals(hex_string: &[HexToken]) -> Option<Vec<u8>> {
+    let mut literals = Vec::new();
+
+    for token in hex_string {
+        match token {
+            HexToken::Byte(b) => literals.push(*b),
+            HexToken::Jump(_) => return None,
+            // TODO: both of those could be handled
+            HexToken::MaskedByte(_, _) => return None,
+            HexToken::Alternatives(_) => return None,
+        }
+    }
+
+    Some(literals)
+}
+
+fn hex_string_to_ast(hex_string: Vec<HexToken>) -> Node {
+    Node::Concat(hex_string.into_iter().map(hex_token_to_ast).collect())
+}
+
+fn hex_token_to_ast(token: HexToken) -> Node {
+    match token {
+        HexToken::Byte(b) => Node::Literal(b),
+        HexToken::MaskedByte(b, mask) => match mask {
+            HexMask::Left => Node::Class(ClassKind::Bracketed(BracketedClass {
+                items: (0..=0xF)
+                    .map(|i| BracketedClassItem::Literal((i << 4) + b))
+                    .collect(),
+                negated: false,
+            })),
+            HexMask::Right => {
+                let b = b << 4;
+                Node::Class(ClassKind::Bracketed(BracketedClass {
+                    items: vec![BracketedClassItem::Range(b, b + 0x0F)],
+                    negated: false,
+                }))
+            }
+            HexMask::All => Node::Dot,
+        },
+        HexToken::Jump(jump) => {
+            let kind = match (jump.from, jump.to) {
+                (from, None) => RepetitionKind::Range(RepetitionRange::AtLeast(from)),
+                (from, Some(to)) => RepetitionKind::Range(RepetitionRange::Bounded(from, to)),
+            };
+            Node::Repetition {
+                node: Box::new(Node::Dot),
+                kind,
+                greedy: false,
+            }
+        }
+        HexToken::Alternatives(elems) => Node::Group(Box::new(Node::Alternation(
+            elems.into_iter().map(hex_string_to_ast).collect(),
+        ))),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use boreal_parser::{parse_str, VariableDeclarationValue};
+
+    #[track_caller]
+    fn test(hex_string: &str, expected_regex: &str) {
+        let rule_str = format!("rule a {{ strings: $a = {} condition: $a }}", hex_string);
+        let mut file = parse_str(&rule_str).unwrap();
+        let mut rule = file
+            .components
+            .pop()
+            .map(|v| match v {
+                boreal_parser::YaraFileComponent::Rule(v) => v,
+                _ => panic!(),
+            })
+            .unwrap();
+        let var = rule.variables.pop().unwrap();
+        let hex_string = match var.value {
+            VariableDeclarationValue::HexString(s) => s,
+            _ => panic!(),
+        };
+
+        let ast = hex_string_to_ast(hex_string);
+        let mut regex = String::new();
+        add_ast_to_string(ast, &mut regex);
+        assert_eq!(regex, expected_regex);
+    }
+
+    #[test]
+    fn test_hex_string_to_regex() {
+        test(
+            "{ AB ?D 01 }",
+            r"\xab[\x0d\x1d\x2d=M\x5dm\x7d\x8d\x9d\xad\xbd\xcd\xdd\xed\xfd]\x01",
+        );
+        test("{ C7 [-] ?? }", r"\xc7.{0,}?.");
+        test(
+            "{ C7 [3-] 5? 03 [-6] C7 ( FF 15 | E8 ) [4] 6A ( FF D? | E8 [2-4] ??) }",
+            r"\xc7.{3,}?[P-_]\x03.{0,6}?\xc7(\xff\x15|\xe8).{4,4}?j(\xff[\xd0-\xdf]|\xe8.{2,4}?.)",
+        );
+    }
+}
