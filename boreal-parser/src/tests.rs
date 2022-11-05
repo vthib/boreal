010e037e93505fba1d65a7d10fff517ad02bc771
Vincent Thiberville <vthib@pm.me>
2022-11-05 15:30:29 +0100

    chore: remove unused test in boreal-parser

--- boreal-parser/src/tests.rs
+++ boreal-parser/src/tests.rs
@@ -1,11 +1,4 @@
 use super::types::{Input, ParseResult};
-use codespan_reporting::{
-    files::SimpleFile,
-    term::{
-        self,
-        termcolor::{ColorChoice, StandardStream},
-    },
-};
 use nom::Finish;
 
 #[track_caller]
@@ -41,44 +34,4 @@ where
     let input = Input::new(input);
     let res = f(input).finish();
     check(res.unwrap().1);
-}
-
-#[test]
-fn test_parsing_global() {
-    let assets_dir = concat!(env!("CARGO_MANIFEST_DIR"), "/tests/parsing/");
-    let glob1 =
-        glob::glob(&format!("{}/**/*.yara", assets_dir)).expect("Failed to read glob pattern");
-    let glob2 =
-        glob::glob(&format!("{}/**/*.yar", assets_dir)).expect("Failed to read glob pattern");
-
-    let writer = StandardStream::stderr(ColorChoice::Always);
-    let config = codespan_reporting::term::Config::default();
-
-    let mut nb_ok = 0;
-    let mut nb_failed = 0;
-    for entry in glob1.chain(glob2) {
-        let entry = entry.unwrap();
-        let contents = std::fs::read_to_string(&entry).unwrap();
-        match super::parse_str(&contents) {
-            Ok(_) => {
-                nb_ok += 1;
-                println!("OK   {:?}", &entry);
-            }
-            Err(e) => {
-                nb_failed += 1;
-                println!("FAIL {:?}", &entry);
-
-                let filename = entry.display().to_string();
-                let files = SimpleFile::new(&filename, &contents);
-                term::emit(&mut writer.lock(), &config, &files, &e.to_diagnostic()).unwrap();
-            }
-        };
-    }
-
-    println!(
-        "parsed {}/{} OK ({:.2}%)",
-        nb_ok,
-        (nb_ok + nb_failed),
-        (f64::from(nb_ok) * 100.) / f64::from(nb_ok + nb_failed)
-    );
 }
