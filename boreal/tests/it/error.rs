19dcccc8ffa08307a531dd5f7be910dbfcff14e8
Vincent Thiberville <vthib@pm.me>
2022-11-07 22:07:14 +0100

    test: fix error test failing on windows for libyara compat

--- boreal/tests/it/error.rs
+++ boreal/tests/it/error.rs
@@ -1,4 +1,4 @@
-use crate::utils::check_err;
+use crate::utils::{check_err, check_err_without_yara};
 
 #[test]
 fn test_invalid_files() {
@@ -6,12 +6,20 @@ fn test_invalid_files() {
     // display of errors. This provides a nice way to check spans and stuff on those errors...
     for file in glob::glob("tests/assets/invalid_files/**/*.yar").unwrap() {
         let file = file.unwrap();
+
         let contents = std::fs::read_to_string(&file)
             .unwrap_or_else(|e| panic!("cannot read file {:?}: {}", file, e));
 
         println!("checking file {:?}", file);
-        // Maybe including the expected prefix in each file (as a comment) would be a nice
-        // addition.
-        check_err(&contents, "");
+        if file.to_string_lossy().contains("str_to_int_range") {
+            // Disable this test against yara because of
+            // https://github.com/VirusTotal/yara/issues/1791, yara returning an error depends
+            // on the platform
+            check_err_without_yara(&contents, "");
+        } else {
+            // Maybe including the expected prefix in each file (as a comment) would be a nice
+            // addition.
+            check_err(&contents, "");
+        }
     }
 }
