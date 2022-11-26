8bac4bc710f29d099909d75f3e73de4db5875f3a
Vincent Thiberville <vthib@pm.me>
2022-11-26 01:39:54 +0100

    bench: improve benches, add more assets

--- boreal/benches/benches.rs
+++ boreal/benches/benches.rs
@@ -1,27 +1,50 @@
 //! Benchmarks boreal against libyara
+use std::path::PathBuf;
+
 use criterion::{criterion_group, criterion_main, Criterion};
 use walkdir::WalkDir;
 
+const RULES_SETS: [(&str, &str); 7] = [
+    // 100 rules, 1998 variables
+    ("panopticon", "benches/assets/panopticon/baseline_100.yar"),
+    // 4297 rules, 23630 vars
+    ("signature-base", "benches/assets/signature-base/yara"),
+    // 147 rules, 644 variables
+    ("orion", "benches/assets/orion"),
+    // 632 rules, 1536 variables
+    ("reversinglabs", "benches/assets/reversinglabs"),
+    // 167 rules, 1408 variables
+    ("atr", "benches/assets/atr"),
+    // 16431 rules, 13155 variables
+    ("icewater", "benches/assets/icewater"),
+    // 121 rules, 5390 variables
+    ("c0ffee", "benches/assets/c0ffee/rules.yar"),
+    // Extremely slow
+    // ("benches/assets/yara-rules/index.yar", 12771),
+];
+
 /// Bench parsing + compilation duration
 fn bench_compilation(c: &mut Criterion) {
-    for (path, nb_rules) in &[
-        ("benches/assets/panopticon/baseline_50.yar", 50),
-        ("benches/assets/panopticon/baseline_100.yar", 100),
-    ] {
-        let rules = std::fs::read_to_string(path).unwrap();
+    for (name, rules_path) in &RULES_SETS {
+        let rules = get_yara_files_from_path(rules_path);
 
-        let mut group = c.benchmark_group(format!("Parse and compile {} rules", nb_rules));
+        let mut group = c.benchmark_group(format!("Parse and compile {}", name));
+        group.sample_size(20);
         group.bench_with_input("boreal", &rules, |b, rules| {
             b.iter_with_large_drop(|| {
-                let mut compiler = boreal::Compiler::new();
-                compiler.add_rules_str(rules).unwrap();
+                let mut compiler = build_boreal_compiler();
+                for path in rules {
+                    compiler.add_rules_file(&path).unwrap();
+                }
                 compiler.into_scanner()
             })
         });
         group.bench_with_input("libyara", &rules, |b, rules| {
             b.iter_with_large_drop(|| {
-                let compiler = yara::Compiler::new().unwrap();
-                let compiler = compiler.add_rules_str(rules).unwrap();
+                let mut compiler = build_yara_compiler();
+                for path in rules {
+                    compiler = compiler.add_rules_file(&path).unwrap();
+                }
                 compiler.compile_rules().unwrap()
             })
         });
@@ -30,26 +53,26 @@ fn bench_compilation(c: &mut Criterion) {
     }
 }
 
-/// Bench scanning a file filled with nul bytes
-fn bench_scan_nul_file(c: &mut Criterion) {
-    for (path, nb_rules) in [
-        ("benches/assets/panopticon/baseline_50.yar", 50),
-        ("benches/assets/panopticon/baseline_100.yar", 100),
-        ("benches/assets/signature-base/yara", 5083),
-    ] {
-        // Test 100B, 200KB, 50MB
-        for file_size in [100, 200 * 1024, 50 * 1024 * 1024] {
-            let mem = vec![0; file_size];
+/// Bench scanning single PE files with different amount of rules
+fn bench_scan_pes(c: &mut Criterion) {
+    for (name, rules_path) in &RULES_SETS {
+        // Test files in assets/pes
+        for pe_path in glob::glob("benches/assets/pes/*").unwrap() {
+            let pe_path = pe_path.unwrap();
+            let mem = std::fs::read(&pe_path).expect("can read asset file");
 
-            let mut boreal_compiler = boreal::Compiler::new();
-            let yara_compiler = yara::Compiler::new().unwrap();
-            let yara_compiler = add_rules(path, &mut boreal_compiler, yara_compiler);
+            let mut boreal_compiler = build_boreal_compiler();
+            let yara_compiler = build_yara_compiler();
+
+            let rules = get_yara_files_from_path(rules_path);
+            let yara_compiler = add_rules(&rules, &mut boreal_compiler, yara_compiler);
 
             let boreal_scanner = boreal_compiler.into_scanner();
             let yara_compiled_rules = yara_compiler.compile_rules().unwrap();
 
             let mut group =
-                c.benchmark_group(format!("Scan {}B with {} rules", file_size, nb_rules));
+                c.benchmark_group(format!("Scan {} using {} rules", pe_path.display(), name));
+            group.sample_size(20);
             group.bench_with_input("boreal", &(boreal_scanner, &mem), |b, (scanner, mem)| {
                 b.iter(|| scanner.scan_mem(mem))
             });
@@ -62,43 +85,76 @@ fn bench_scan_nul_file(c: &mut Criterion) {
     }
 }
 
+fn build_boreal_compiler() -> boreal::Compiler {
+    let mut boreal_compiler = boreal::Compiler::new();
+    let _ = boreal_compiler.define_symbol("owner", "owner");
+    let _ = boreal_compiler.define_symbol("filename", "filename");
+    let _ = boreal_compiler.define_symbol("filepath", "filepath");
+    let _ = boreal_compiler.define_symbol("extension", "bin");
+    let _ = boreal_compiler.define_symbol("filetype", "bin");
+    boreal_compiler
+}
+
+fn build_yara_compiler() -> yara::Compiler {
+    let mut yara_compiler = yara::Compiler::new().unwrap();
+    let _ = yara_compiler.define_variable("owner", "owner");
+    let _ = yara_compiler.define_variable("filename", "filename");
+    let _ = yara_compiler.define_variable("filepath", "filepath");
+    let _ = yara_compiler.define_variable("extension", "bin");
+    let _ = yara_compiler.define_variable("filetype", "bin");
+    yara_compiler
+}
+
+fn get_yara_files_from_path(path: &str) -> Vec<PathBuf> {
+    WalkDir::new(path)
+        .into_iter()
+        .filter_map(|entry| {
+            let entry = entry.unwrap();
+            let path = entry.path();
+
+            if entry.file_type().is_dir() {
+                return None;
+            }
+
+            match entry.path().extension() {
+                Some(v) => {
+                    let ext = v.to_string_lossy();
+                    if ext != "yar" && ext != "yara" {
+                        return None;
+                    }
+                }
+                None => return None,
+            }
+
+            Some(path.to_path_buf())
+        })
+        .collect()
+}
+
 fn add_rules(
-    path: &str,
+    rules: &[PathBuf],
     boreal_compiler: &mut boreal::Compiler,
     mut yara_compiler: yara::Compiler,
 ) -> yara::Compiler {
-    for entry in WalkDir::new(path) {
-        let entry = entry.unwrap();
-        let path = entry.path();
-
-        if entry.file_type().is_dir() {
-            continue;
-        }
-
-        let contents = std::fs::read_to_string(path).unwrap();
-
-        boreal_compiler
-            .add_rules_str(&contents)
-            .unwrap_or_else(|err| {
-                panic!(
-                    "cannot parse rules from {} for boreal: {:?}",
-                    path.display(),
-                    err
-                )
-            });
-        yara_compiler = yara_compiler
-            .add_rules_str(&contents)
-            .unwrap_or_else(|err| {
-                panic!(
-                    "cannot parse rules from {} for libyara: {:?}",
-                    path.display(),
-                    err
-                )
-            });
+    for path in rules {
+        boreal_compiler.add_rules_file(&path).unwrap_or_else(|err| {
+            panic!(
+                "cannot parse rules from {} for boreal: {:?}",
+                path.display(),
+                err
+            )
+        });
+        yara_compiler = yara_compiler.add_rules_file(&path).unwrap_or_else(|err| {
+            panic!(
+                "cannot parse rules from {} for libyara: {:?}",
+                path.display(),
+                err
+            )
+        });
     }
 
     yara_compiler
 }
 
-criterion_group!(benches, bench_compilation, bench_scan_nul_file);
+criterion_group!(benches, bench_compilation, bench_scan_pes);
 criterion_main!(benches);
--- boreal/benches/retrieve_assets.sh
+++ boreal/benches/retrieve_assets.sh
@@ -1,11 +1,43 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 #
 # Retrieve assets used in benches
 
-set -ex
+set -e
 
-git clone git@github.com:Neo23x0/panopticon.git assets/panopticon
-cd assets/panopticon && git checkout f3aee9296deb4b09cbce75450526883e04add529 && cd ../..
+mkdir assets
 
-git clone git@github.com:Neo23x0/signature-base.git assets/signature-base
-cd assets/signature-base && git checkout 459fe4de6ddadfe975ad6d2e7e61b97a45eaaaa5 && cd ../..
+checkout() {
+    echo "retrieving $2..."
+    git clone --depth 1 -q $1 assets/$2
+    pushd assets/$2
+    git checkout -q $3
+    popd
+}
+
+# Checkout sources of yara rules
+
+checkout git@github.com:Neo23x0/panopticon.git panopticon f3aee9296deb4b09cbce75450526883e04add529
+checkout git@github.com:Neo23x0/signature-base.git signature-base 459fe4de6ddadfe975ad6d2e7e61b97a45eaaaa5
+checkout git@github.com:StrangerealIntel/Orion.git orion cbdebf8116cb017f944123438b700d562c388b02
+checkout git@github.com:Yara-Rules/rules.git yara-rules 0f93570194a80d2f2032869055808b0ddcdfb360
+# Remove one rule that depends on the cuckoo module
+sed -ie 's!include "./malware/MALW_AZORULT.yar"!!' assets/yara-rules/index.yar
+checkout git@github.com:reversinglabs/reversinglabs-yara-rules.git reversinglabs 6aacf65eb2648ca7e6bc8767ffb2fdf032951dd6
+checkout git@github.com:advanced-threat-research/Yara-Rules.git atr 4e29051f9c6d80a2d9f4b33ada7f7b377a8d9f4f
+checkout git@github.com:SupportIntelligence/Icewater.git icewater 71e327039a6cfeee4bcfc045f3c6d5d039ffee78
+checkout git@github.com:Crypt-0n/C0-FF-EE.git c0ffee 4704222f828278eeec665234ea0d2166afe964ca
+
+# Retrieve binaries to scan against
+wget https://storage.googleapis.com/chromium-browser-snapshots/Win/1061148/chrome-win.zip
+unzip chrome-win.zip
+mkdir -p assets/pes
+# One small file, 737KB
+cp chrome-win/vulkan-1.dll assets/pes
+# One bigger file, 5.5MB
+cp chrome-win/libGLESv2.dll assets/pes
+# One huge file, 157M
+cp chrome-win/interactive_ui_tests.exe assets/pes
+rm -rf chrome-win chrome-win.zip
+
+wget "https://download-installer.cdn.mozilla.net/pub/firefox/releases/107.0/win64/en-US/Firefox Setup 107.0.msi"
+mv "Firefox Setup 107.0.msi" assets/pes/firefox.msi
