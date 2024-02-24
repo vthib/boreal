2445cf3588bc4be467272908b6a7c6718548a866
Vincent Thiberville <vthib@pm.me>
2024-02-25 00:54:01 +0100

    feat: start implementing resources for dotnet module

--- boreal/tests/it/dotnet.rs
+++ boreal/tests/it/dotnet.rs
@@ -10,11 +10,9 @@ fn test_coverage_0ca09bde() {
         "dotnet.version",
         "dotnet.classes",
         "dotnet.number_of_classes",
-        "dotnet.number_of_resources",
         "dotnet.number_of_streams",
         "dotnet.streams",
         "dotnet.module_name",
-        "dotnet.resources",
     ];
     let path = "tests/assets/libyara/data/0ca09bde7602769120fadc4f7a4147347a7a97271370583586c9e587fd396171";
     compare_module_values_on_file(Dotnet, path, false, &diffs);
@@ -28,9 +26,7 @@ fn test_coverage_756684f4() {
         "dotnet.number_of_constants",
         "dotnet.classes",
         "dotnet.number_of_classes",
-        "dotnet.number_of_resources",
         "dotnet.module_name",
-        "dotnet.resources",
     ];
     let path = "tests/assets/libyara/data/756684f4017ba7e931a26724ae61606b16b5f8cc84ed38a260a34e50c5016f59";
     compare_module_values_on_file(Dotnet, path, false, &diffs);
@@ -45,11 +41,9 @@ fn test_coverage_bad_dotnet_pe() {
         "dotnet.version",
         "dotnet.classes",
         "dotnet.number_of_classes",
-        "dotnet.number_of_resources",
         "dotnet.number_of_streams",
         "dotnet.streams",
         "dotnet.module_name",
-        "dotnet.resources",
     ];
     let path = "tests/assets/libyara/data/bad_dotnet_pe";
     compare_module_values_on_file(Dotnet, path, false, &diffs);
