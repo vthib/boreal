1d09e6ece70b824629691095b0ada439d27ec8e8
Vincent Thiberville <vthib@pm.me>
2024-02-26 01:48:56 +0100

    feat: finish implementing resources in dotnet module

--- boreal/tests/it/dotnet.rs
+++ boreal/tests/it/dotnet.rs
@@ -4,15 +4,7 @@ use crate::utils::compare_module_values_on_file;
 
 #[test]
 fn test_coverage_0ca09bde() {
-    let diffs = [
-        "dotnet.constants",
-        "dotnet.is_dotnet",
-        "dotnet.version",
-        "dotnet.classes",
-        "dotnet.number_of_classes",
-        "dotnet.number_of_streams",
-        "dotnet.streams",
-    ];
+    let diffs = ["dotnet.classes", "dotnet.number_of_classes"];
     let path = "tests/assets/libyara/data/0ca09bde7602769120fadc4f7a4147347a7a97271370583586c9e587fd396171";
     compare_module_values_on_file(Dotnet, path, false, &diffs);
     compare_module_values_on_file(Dotnet, path, true, &diffs);
@@ -20,12 +12,7 @@ fn test_coverage_0ca09bde() {
 
 #[test]
 fn test_coverage_756684f4() {
-    let diffs = [
-        "dotnet.constants",
-        "dotnet.number_of_constants",
-        "dotnet.classes",
-        "dotnet.number_of_classes",
-    ];
+    let diffs = ["dotnet.classes", "dotnet.number_of_classes"];
     let path = "tests/assets/libyara/data/756684f4017ba7e931a26724ae61606b16b5f8cc84ed38a260a34e50c5016f59";
     compare_module_values_on_file(Dotnet, path, false, &diffs);
     compare_module_values_on_file(Dotnet, path, true, &diffs);
@@ -33,15 +20,7 @@ fn test_coverage_756684f4() {
 
 #[test]
 fn test_coverage_bad_dotnet_pe() {
-    let diffs = [
-        "dotnet.constants",
-        "dotnet.is_dotnet",
-        "dotnet.version",
-        "dotnet.classes",
-        "dotnet.number_of_classes",
-        "dotnet.number_of_streams",
-        "dotnet.streams",
-    ];
+    let diffs = ["dotnet.classes", "dotnet.number_of_classes"];
     let path = "tests/assets/libyara/data/bad_dotnet_pe";
     compare_module_values_on_file(Dotnet, path, false, &diffs);
     compare_module_values_on_file(Dotnet, path, true, &diffs);
