252e163795e414cca583a2fb7baed33839466bd8
Vincent Thiberville <vthib@pm.me>
2024-04-04 00:45:02 +0200

    test: add some more tests on parsing for dotnet module
    Add a file to test assembly parsing, and fix a bug in setting the
    number_of_resources key

--- boreal/tests/it/dotnet.rs
+++ boreal/tests/it/dotnet.rs
@@ -1,6 +1,124 @@
 use boreal::module::Dotnet;
 
-use crate::utils::{compare_module_values_on_file, Checker};
+use crate::utils::{check_file, compare_module_values_on_file, Checker};
+
+#[test]
+fn test_streams() {
+    check_file(
+        r##"
+import "dotnet"
+rule test {
+    condition:
+        dotnet.version == "v2.0.50727" and
+        dotnet.number_of_streams == 5 and
+
+        dotnet.streams[0].name == "#~" and
+        dotnet.streams[0].offset == 1012 and
+        dotnet.streams[0].size == 244 and
+
+        dotnet.streams[1].name == "#Strings" and
+        dotnet.streams[1].offset == 1256 and
+        dotnet.streams[1].size == 316 and
+
+        dotnet.streams[2].name == "#US" and
+        dotnet.streams[2].offset == 1572 and
+        dotnet.streams[2].size == 8 and
+
+        dotnet.streams[3].name == "#GUID" and
+        dotnet.streams[3].offset == 1580 and
+        dotnet.streams[3].size == 16 and
+
+        dotnet.streams[4].name == "#Blob" and
+        dotnet.streams[4].offset == 1596 and
+        dotnet.streams[4].size == 204
+}"##,
+        "tests/assets/libyara/data/\
+         0ca09bde7602769120fadc4f7a4147347a7a97271370583586c9e587fd396171",
+        true,
+    );
+}
+
+#[test]
+fn test_assembly() {
+    let mut checker = Checker::new(
+        r##"
+import "dotnet"
+rule assembly {
+    condition:
+        dotnet.assembly.name == "?iD.gabu.Zo$.@mEu" and
+        dotnet.assembly.culture == "de-ch" and
+        dotnet.assembly.version.major == 57 and
+        dotnet.assembly.version.minor == 239 and
+        dotnet.assembly.version.build_number == 120 and
+        dotnet.assembly.version.revision_number == 17706
+}
+
+rule ar0 {
+    condition:
+        dotnet.assembly_refs[0].name == "Kwee" and
+        dotnet.assembly_refs[0].version.major == 0 and
+        dotnet.assembly_refs[0].version.minor == 0 and
+        dotnet.assembly_refs[0].version.build_number == 0 and
+        dotnet.assembly_refs[0].version.revision_number == 0 and
+        not defined dotnet.assembly_refs[0].public_key_or_token
+}
+
+rule ar1 {
+    condition:
+        dotnet.assembly_refs[1].name == "one.two.three" and
+        dotnet.assembly_refs[1].version.major == 0 and
+        dotnet.assembly_refs[1].version.minor == 0 and
+        dotnet.assembly_refs[1].version.build_number == 0 and
+        dotnet.assembly_refs[1].version.revision_number == 0 and
+        not defined dotnet.assembly_refs[1].public_key_or_token
+}
+
+rule ar2 {
+    condition:
+        dotnet.assembly_refs[2].name == "four.five" and
+        dotnet.assembly_refs[2].public_key_or_token ==
+            "\xde\x45\x23\x89\x82\x06\x07\xa6\xbb\xaa\xbb\xee\x11\x22\x33\x00" and
+        dotnet.assembly_refs[2].version.major == 0 and
+        dotnet.assembly_refs[2].version.minor == 0 and
+        dotnet.assembly_refs[2].version.build_number == 0 and
+        dotnet.assembly_refs[2].version.revision_number == 0
+}
+
+rule ar3 {
+    condition:
+        dotnet.assembly_refs[3].name == "muwu" and
+        dotnet.assembly_refs[3].version.major == 0 and
+        dotnet.assembly_refs[3].version.minor == 0 and
+        dotnet.assembly_refs[3].version.build_number == 0 and
+        dotnet.assembly_refs[3].version.revision_number == 0 and
+        not defined dotnet.assembly_refs[3].public_key_or_token
+}
+
+rule ar4 {
+    condition:
+        dotnet.assembly_refs[4].name == "in external.Assembly@" and
+        dotnet.assembly_refs[4].public_key_or_token ==
+            "\xbb\xaa\xbb\xee\x11\x22\x33\x00" and
+        dotnet.assembly_refs[4].version.major == 912 and
+        dotnet.assembly_refs[4].version.minor == 35720 and
+        dotnet.assembly_refs[4].version.build_number == 283 and
+        dotnet.assembly_refs[4].version.revision_number == 212
+}"##,
+    );
+
+    let mem = std::fs::read("tests/assets/dotnet/assembly.dll").unwrap();
+    checker.check_rule_matches(
+        &mem,
+        &[
+            "default:assembly",
+            "default:ar0",
+            "default:ar1",
+            "default:ar2",
+            "default:ar3",
+            "default:ar4",
+        ],
+    );
+}
 
 #[test]
 fn test_types_exe() {
@@ -343,6 +461,16 @@ fn test_coverage_types2() {
         "dotnet.classes[1].methods[2].parameters[3].type",
     ];
     let path = "tests/assets/dotnet/types2.dll";
+    compare_module_values_on_file(Dotnet, path, false, &diffs);
+
+    // DLL so not considered when scanning as a process memory
+    compare_module_values_on_file(Dotnet, path, true, &[]);
+}
+
+#[test]
+fn test_coverage_assembly() {
+    let diffs = [];
+    let path = "tests/assets/dotnet/assembly.dll";
     compare_module_values_on_file(Dotnet, path, false, &diffs);
 
     // DLL so not considered when scanning as a process memory
