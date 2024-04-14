9277c7e2ce0e3d147c95a50bac805ac3fe5a0849
Vincent Thiberville <vthib@pm.me>
2024-04-14 17:10:24 +0200

    feat: add cache in magic module

--- boreal/src/module/magic.rs
+++ boreal/src/module/magic.rs
@@ -1,8 +1,8 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, sync::RwLock};
 
 use magic::cookie::{Cookie, DatabasePaths, Flags};
 
-use super::{EvalContext, Module, StaticValue, Type, Value};
+use super::{EvalContext, Module, ModuleData, ModuleDataMap, StaticValue, Type, Value};
 
 /// `magic` module.
 #[derive(Debug)]
@@ -26,15 +26,64 @@ impl Module for Magic {
         ]
         .into()
     }
+
+    fn setup_new_scan(&self, data_map: &mut ModuleDataMap) {
+        data_map.insert::<Self>(Data {
+            cache: RwLock::new(Cache {
+                magic_mime_type: CacheEntry::NotComputed,
+                magic_type: CacheEntry::NotComputed,
+            }),
+        });
+    }
+}
+
+pub struct Data {
+    cache: RwLock<Cache>,
+}
+
+pub struct Cache {
+    magic_mime_type: CacheEntry,
+    magic_type: CacheEntry,
+}
+
+pub enum CacheEntry {
+    NotComputed,
+    Computed(Option<Value>),
+}
+
+impl ModuleData for Magic {
+    type Data = Data;
 }
 
 impl Magic {
     fn get_mime_type(ctx: &mut EvalContext, _: Vec<Value>) -> Option<Value> {
-        get_magic_value(ctx, Flags::MIME_TYPE)
+        let data = ctx.module_data.get::<Self>()?;
+
+        {
+            let cache = data.cache.read().ok()?;
+            if let CacheEntry::Computed(v) = &cache.magic_mime_type {
+                return v.clone();
+            }
+        }
+
+        let res = get_magic_value(ctx, Flags::MIME_TYPE);
+        data.cache.write().ok()?.magic_mime_type = CacheEntry::Computed(res.clone());
+        res
     }
 
     fn get_type(ctx: &mut EvalContext, _: Vec<Value>) -> Option<Value> {
-        get_magic_value(ctx, Flags::default())
+        let data = ctx.module_data.get::<Self>()?;
+
+        {
+            let cache = data.cache.read().ok()?;
+            if let CacheEntry::Computed(v) = &cache.magic_type {
+                return v.clone();
+            }
+        }
+
+        let res = get_magic_value(ctx, Flags::default());
+        data.cache.write().ok()?.magic_type = CacheEntry::Computed(res.clone());
+        res
     }
 }
 
