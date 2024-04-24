4b0f4a13c7f93d12d963538bc5bdc9ee8fae5a2d
Vincent Thiberville <vthib@pm.me>
2024-04-25 01:10:20 +0200

    feat: implement has_method/has_class functions in dex module

--- boreal/tests/it/dex.rs
+++ boreal/tests/it/dex.rs
@@ -1,11 +1,63 @@
 use boreal::module::Dex;
 
 use crate::libyara_compat::util::DEX_FILE;
-use crate::utils::compare_module_values_on_mem;
+use crate::utils::{check, compare_module_values_on_mem};
 
 #[test]
-fn test_coverage_constants() {
+fn test_coverage_dex_file() {
     let diffs = [];
     compare_module_values_on_mem(Dex, "DEX_FILE", DEX_FILE, false, &diffs);
     compare_module_values_on_mem(Dex, "DEX_FILE", DEX_FILE, true, &diffs);
+}
+
+#[track_caller]
+fn test_dex_file(cond: &str) {
+    check(
+        &format!("import \"dex\" rule test {{ condition: {cond} }}"),
+        DEX_FILE,
+        true,
+    );
+}
+
+#[test]
+fn test_has_method() {
+    // class_name, method_name
+    test_dex_file(r#"dex.has_method("Lcom/android/tools/ir/server/AppInfo;", "<clinit>") == 1"#);
+    test_dex_file(r#"dex.has_method("LCOM/android/tools/ir/server/AppInfo;", "<clinit>") == 0"#);
+    test_dex_file(r#"dex.has_method("Lcom/android/tools/ir/server/AppInfo;", "<CLINIT>") == 0"#);
+    test_dex_file(r#"dex.has_method("Lcom", "<clinit>") == 0"#);
+    test_dex_file(r#"dex.has_method("Lcom", "<init>") == 0"#);
+    test_dex_file(r#"dex.has_method("Lcom/android/tools/ir/server/AppInfo;", "<init>") == 1"#);
+    test_dex_file(r#"dex.has_method("Lcom/android/tools/ir/server/AppInfo;", "<it>") == 0"#);
+
+    // method_name
+    test_dex_file(r#"dex.has_method("<clinit>") == 1"#);
+    test_dex_file(r#"dex.has_method("<linit>") == 0"#);
+    test_dex_file(r#"dex.has_method("<init>") == 1"#);
+    test_dex_file(r#"dex.has_method("<init") == 0"#);
+
+    // class_name_regex, method_name_regex
+    test_dex_file(r#"dex.has_method(/Lcom\/.*/, /<\w+>/) == 1"#);
+    test_dex_file(r#"dex.has_method(/lcom\/.*/, /<\w+>/) == 0"#);
+    test_dex_file(r#"dex.has_method(/Lcom\//, /<\w+/) == 1"#);
+
+    // method_name_regex
+    test_dex_file(r#"dex.has_method(/<\w+>/) == 1"#);
+    test_dex_file(r#"dex.has_method(/<cl.*/) == 1"#);
+    test_dex_file(r#"dex.has_method(/<\w+/) == 1"#);
+    test_dex_file(r#"dex.has_method(/foo/) == 0"#);
+}
+
+#[test]
+fn test_has_class() {
+    // class_name
+    test_dex_file(r#"dex.has_class("Lcom/android/tools/ir/server/AppInfo;") == 1"#);
+    test_dex_file(r#"dex.has_class("LCOM/android/tools/ir/server/AppInfo;") == 0"#);
+    test_dex_file(r#"dex.has_class("Lcom/") == 0"#);
+
+    // class_name_regex
+    test_dex_file(r#"dex.has_class(/^Lcom/) == 1"#);
+    test_dex_file(r#"dex.has_class(/^lcom/) == 0"#);
+    test_dex_file(r#"dex.has_class(/\w{7}\/\w{5}/) == 1"#);
+    test_dex_file(r#"dex.has_class(/\/\w{3}\//) == 0"#);
 }
