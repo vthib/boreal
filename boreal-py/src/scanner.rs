df9926d21977c1a95dfa7df61b987e86dddf7a5f
Vincent Thiberville <vthib@pm.me>
2025-01-30 21:02:42 +0100

    feat[py]: implement more options for match method

--- boreal-py/src/scanner.rs
+++ boreal-py/src/scanner.rs
@@ -1,49 +1,138 @@
-use pyo3::exceptions::PyValueError;
-use pyo3::prelude::*;
-use pyo3::types::{PyBytes, PyString};
+use std::time::Duration;
 
-use ::boreal::Scanner;
+use pyo3::create_exception;
+use pyo3::exceptions::{PyException, PyTypeError};
+use pyo3::prelude::*;
+use pyo3::types::PyDict;
+
+use ::boreal::scanner;
 
 use crate::rule_match::Match;
 
+create_exception!(boreal, ScanError, PyException, "error when scanning");
+create_exception!(boreal, TimeoutError, PyException, "scan timed out");
+
 #[pyclass]
-pub struct PyScanner {
-    pub scanner: Scanner,
+pub struct Scanner {
+    pub scanner: scanner::Scanner,
     #[pyo3(get)]
     pub warnings: Vec<String>,
 }
 
 #[pymethods]
-impl PyScanner {
-    #[pyo3(signature = (filepath=None, data=None))]
+impl Scanner {
+    #[allow(clippy::too_many_arguments)]
+    #[pyo3(signature = (
+        filepath=None,
+        data=None,
+        pid=None,
+        externals=None,
+        callback=None,
+        fast=false,
+        timeout=None,
+        modules_data=None,
+        modules_callback=None,
+        warnings_callback=None,
+        which_callbacks=None,
+        console_callback=None,
+    ))]
     fn r#match(
         &self,
         filepath: Option<&str>,
         data: Option<&Bound<'_, PyAny>>,
+        pid: Option<u32>,
+        externals: Option<&Bound<'_, PyDict>>,
+        callback: Option<&Bound<'_, PyAny>>,
+        fast: bool,
+        timeout: Option<u64>,
+        modules_data: Option<&Bound<'_, PyDict>>,
+        modules_callback: Option<&Bound<'_, PyAny>>,
+        warnings_callback: Option<&Bound<'_, PyAny>>,
+        which_callbacks: Option<&Bound<'_, PyAny>>,
+        console_callback: Option<&Bound<'_, PyAny>>,
     ) -> PyResult<Vec<Match>> {
-        let res = match (filepath, data) {
-            (Some(filepath), None) => self.scanner.scan_file(filepath),
-            (None, Some(data)) => {
-                if let Ok(s) = data.downcast::<PyString>() {
-                    self.scanner.scan_mem(s.to_str()?.as_bytes())
-                } else if let Ok(s) = data.downcast::<PyBytes>() {
-                    self.scanner.scan_mem(s.as_bytes())
+        let mut scanner = self.scanner.clone();
+
+        if let Some(externals) = externals {
+            set_externals(&mut scanner, externals)?;
+        }
+
+        if let Some(timeout) = timeout {
+            scanner.set_scan_params(
+                scanner::ScanParams::default().timeout_duration(Some(Duration::from_secs(timeout))),
+            );
+        }
+
+        // TODO
+        {
+            let _ = callback;
+            let _ = fast;
+            let _ = modules_data;
+            let _ = modules_callback;
+            let _ = warnings_callback;
+            let _ = which_callbacks;
+            let _ = console_callback;
+        }
+
+        let res = match (filepath, data, pid) {
+            (Some(filepath), None, None) => scanner.scan_file(filepath),
+            (None, Some(data), None) => {
+                if let Ok(s) = data.extract::<&[u8]>() {
+                    scanner.scan_mem(s)
+                } else if let Ok(s) = data.extract::<&str>() {
+                    scanner.scan_mem(s.as_bytes())
                 } else {
-                    todo!()
+                    return Err(PyTypeError::new_err(
+                        "data must be a string or a bytestring",
+                    ));
                 }
             }
-            _ => todo!(),
+            (None, None, Some(pid)) => scanner.scan_process(pid),
+            _ => {
+                return Err(PyTypeError::new_err(
+                    "one of filepath, data or pid must be passed",
+                ))
+            }
         };
 
         match res {
             Ok(v) => Python::with_gil(|py| {
                 v.matched_rules
                     .into_iter()
-                    .map(|v| Match::new(py, &self.scanner, v))
+                    .map(|v| Match::new(py, &scanner, v))
                     .collect::<Result<_, _>>()
             }),
-            // TODO: fix difference
-            Err((err, _)) => Err(PyValueError::new_err(format!("{err}"))),
+            Err((err, _)) => match err {
+                scanner::ScanError::Timeout => Err(TimeoutError::new_err("")),
+                _ => Err(ScanError::new_err(format!("{err}"))),
+            },
         }
     }
+}
+
+fn set_externals(scanner: &mut scanner::Scanner, externals: &Bound<'_, PyDict>) -> PyResult<()> {
+    for (key, value) in externals {
+        let name: &str = key.extract()?;
+
+        let res = if let Ok(v) = value.extract::<bool>() {
+            scanner.define_symbol(name, v)
+        } else if let Ok(v) = value.extract::<i64>() {
+            scanner.define_symbol(name, v)
+        } else if let Ok(v) = value.extract::<f64>() {
+            scanner.define_symbol(name, v)
+        } else if let Ok(v) = value.extract::<&str>() {
+            scanner.define_symbol(name, v)
+        } else if let Ok(v) = value.extract::<&[u8]>() {
+            scanner.define_symbol(name, v)
+        } else {
+            return Err(PyTypeError::new_err(
+                "invalid type for the external value, must be a boolean, integer, float or string",
+            ));
+        };
+
+        // the error is ignored as is done in YARA: this avoids aborting the scan for an
+        // overfit dictionary.
+        drop(res);
+    }
+    Ok(())
 }
---
+++ boreal-py/tests/test_types.py
@@ -0,0 +1,199 @@
+import boreal
+import pytest
+import yara
+
+
+MODULES = [
+    (boreal, False),
+    (yara, True),
+]
+
+
+@pytest.mark.parametrize("module,is_yara", MODULES)
+def test_match(module, is_yara):
+    """Test all properties related to the Match object"""
+
+    # Check default namespace
+    rule = module.compile(source="rule a { condition: true }")
+    matches = rule.match(data='')
+    assert len(matches) == 1
+    # FIXME
+    assert matches[0].namespace == ('default' if is_yara else '')
+
+    # Check with multiple namespaces
+    source = """
+rule r1: bar baz {
+    meta:
+        s = "a\\nz"
+        b = true
+        v = -11
+    condition:
+        true
+}
+
+rule r2: quux { condition: false }
+rule r3 { condition: true }
+"""
+
+    rules = module.compile(sources={
+        'ns1': source,
+        'ns2': source,
+    })
+    rules2 = module.compile(sources={
+        'ns1': 'rule r1 { condition: true }',
+        'ns2': 'rule r1 { condition: true }',
+    })
+    matches = rules.match(data='')
+    assert len(matches) == 4
+    m0 = matches[0]
+    m1 = matches[1]
+    m2 = matches[2]
+    m3 = matches[3]
+
+    matches2 = rules2.match(data='')
+    assert len(matches2) == 2
+    r2_m0 = matches2[0]
+    r2_m1 = matches2[1]
+
+    assert m0.rule == 'r1'
+    assert m0.namespace == 'ns1'
+    assert m0.tags == ['bar', 'baz']
+    assert m0.meta == {
+        # XXX yara forces a string type, losing information.
+        's': 'a\nz' if is_yara else b'a\nz',
+        'b': True,
+        'v': -11
+    }
+
+    assert m1.rule == 'r3'
+    assert m1.namespace == 'ns1'
+
+    assert m2.rule == 'r1'
+    assert m2.namespace == 'ns2'
+
+    assert m3.rule == 'r3'
+    assert m3.namespace == 'ns2'
+
+    # check special method __repr__
+    assert m0.__repr__() == 'r1'
+    assert m1.__repr__() == 'r3'
+    assert m2.__repr__() == 'r1'
+    assert m3.__repr__() == 'r3'
+    assert r2_m0.__repr__() == 'r1'
+    assert r2_m1.__repr__() == 'r1'
+
+    assert hash(m0) != hash(m1) # rule name differs
+    assert hash(m0) != hash(m2) # namespace name differs
+    assert hash(m0) == hash(r2_m0) # same name and namespace
+    assert hash(m0) != hash(r2_m1)
+
+    # check richcmp impl
+    # eq
+    assert not (m0 == m1)
+    assert not (m0 == m2)
+    assert m0 == r2_m0
+    # ne
+    assert (m0 != m1)
+    assert (m0 != m2)
+    assert not (m0 != r2_m0)
+    # <=
+    assert m0 <= m1
+    assert not (m1 <= m2)
+    assert m0 <= m2
+    assert m0 <= r2_m0
+    # <
+    assert m0 < m1
+    assert not (m1 < m2)
+    assert m0 < m2
+    assert not (m0 < r2_m0)
+    # >=
+    assert m1 >= m0
+    assert not (m2 >= m1)
+    assert m2 >= m0
+    assert r2_m0 >= m0
+    # >
+    assert m1 > m0
+    assert not (m2 > m1)
+    assert m2 > m0
+    assert not (r2_m0 > m0)
+
+
+@pytest.mark.parametrize("module,is_yara", MODULES)
+def test_string_matches(module, is_yara):
+    """Test all properties related to the StringMatches object"""
+    rule = module.compile(source="""
+rule foo {
+    strings:
+        $ = "a"
+        $ = "b"
+        $c = "c"
+    condition:
+        any of them
+}""")
+    matches = rule.match(data=b'abca')
+    assert len(matches) == 1
+    m = matches[0]
+    assert len(m.strings) == 3
+    s0 = m.strings[0]
+    s1 = m.strings[1]
+    s2 = m.strings[2]
+
+    # check standard getters: identifier, instances
+    assert s0.identifier == '$'
+    assert len(s0.instances) == 2
+    assert s1.identifier == '$'
+    assert len(s1.instances) == 1
+    assert s2.identifier == '$c'
+    assert len(s2.instances) == 1
+
+    # check special method __repr__
+    assert s0.__repr__() == '$'
+    assert s1.__repr__() == '$'
+    assert s2.__repr__() == '$c'
+
+    # Check that the hash depends only on the identifier
+    assert hash(s0) == hash(s1)
+
+
+@pytest.mark.parametrize("module,is_yara", MODULES)
+def test_string_match(module, is_yara):
+    """Test all properties related to the StringMatch object"""
+    rule = module.compile(source="""
+rule foo {
+    strings:
+        $ = /<.{1,3}>/
+    condition:
+        any of them
+}""")
+    matches = rule.match(data=b'<a> <\td> <\x00\xFF\xFB> <a>')
+    assert len(matches) == 1
+    m = matches[0]
+    assert len(m.strings) == 1
+    s = m.strings[0]
+    assert len(s.instances) == 4
+    i0 = s.instances[0]
+    i1 = s.instances[1]
+    i2 = s.instances[2]
+    i3 = s.instances[3]
+
+    # check standard getters: offset, matched_data, matched_length
+    assert i0.offset == 0
+    assert i0.matched_length == 3
+    assert i0.matched_data == b'<a>'
+    assert i1.offset == 4
+    assert i1.matched_length == 4
+    assert i1.matched_data == b'<\td>'
+    assert i2.offset == 9
+    assert i2.matched_length == 5
+    assert i2.matched_data == b'<\x00\xFF\xFB>'
+
+    # TODO: missing xor_key and plaintext
+
+    # check special method __repr__
+    assert i0.__repr__() == '<a>'
+    assert i1.__repr__() == '<\td>'
+    # TODO: difference here, should we care about it?
+    assert i2.__repr__() == '<\x00\\xff\\xfb>' if is_yara else '<\x00\uFFFD\uFFFD>'
+
+    # Check that the hash depends only on the matched_data
+    assert hash(i0) == hash(i3)
