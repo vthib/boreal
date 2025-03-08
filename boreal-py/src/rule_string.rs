1a4093d28467163ebe036d54d20a88e18029c76c
Vincent Thiberville <vthib@pm.me>
2025-03-08 16:42:06 +0100

    feat: handle string reached match limit in boreal-py

---
+++ boreal-py/src/rule_string.rs
@@ -0,0 +1,30 @@
+use pyo3::{prelude::*, types::PyString};
+
+use ::boreal::scanner::StringIdentifier;
+
+/// Identifier for a string
+// FIXME: this is a namedtuple in yara
+#[pyclass(frozen, module = "boreal")]
+pub struct RuleString {
+    /// Namespace of the rule containing the string.
+    #[pyo3(get)]
+    namespace: Py<PyString>,
+
+    /// Name of the rle containing the string.
+    #[pyo3(get)]
+    rule: Py<PyString>,
+
+    /// Name of the string.
+    #[pyo3(get)]
+    string: Py<PyString>,
+}
+
+impl RuleString {
+    pub fn new(py: Python, id: &StringIdentifier) -> Self {
+        Self {
+            namespace: PyString::new(py, id.rule_namespace.unwrap_or("default")).unbind(),
+            rule: PyString::new(py, id.rule_name).unbind(),
+            string: PyString::new(py, &format!("${}", id.string_name)).unbind(),
+        }
+    }
+}
