4b89342d82752545464fbce732d10eedf68a73b6
Vincent Thiberville <vthib@pm.me>
2025-05-08 02:08:58 +0200

    bench: move serialize as an optional feature
    This allows measuring its impact on the compiled size.

--- benches/src/serialized_size.rs
+++ benches/src/serialized_size.rs
@@ -1,21 +1,26 @@
-use boreal_benches::{
-    build_boreal_scanner, build_yara_rules, build_yara_x_rules, get_yara_files_from_path,
-    RULES_SETS,
-};
-
 fn main() {
-    for (name, rules_path) in &RULES_SETS {
+    #[cfg(feature = "serialize")]
+    for (name, rules_path) in &boreal_benches::RULES_SETS {
+        use boreal_benches::{
+            build_boreal_scanner, build_yara_rules, build_yara_x_rules, get_yara_files_from_path,
+        };
+
         println!("{name}:");
 
         let rules = get_yara_files_from_path(rules_path);
 
-        let boreal_scanner = build_boreal_scanner(&rules, true);
+        let boreal_speed_scanner = build_boreal_scanner(&rules, true);
+        let boreal_memory_scanner = build_boreal_scanner(&rules, false);
         let mut yara_rules = build_yara_rules(&rules);
         let yara_x_rules = build_yara_x_rules(&rules);
 
         let mut out = Vec::new();
-        boreal_scanner.to_bytes(&mut out).unwrap();
-        println!("boreal: {}", datasize(out.len()));
+        boreal_speed_scanner.to_bytes(&mut out).unwrap();
+        println!("boreal-speed: {}", datasize(out.len()));
+
+        let mut out = Vec::new();
+        boreal_memory_scanner.to_bytes(&mut out).unwrap();
+        println!("boreal-memory: {}", datasize(out.len()));
 
         let mut out = Vec::new();
         yara_rules.save_to_stream(&mut out).unwrap();
@@ -24,10 +29,11 @@ fn main() {
         let out = yara_x_rules.serialize().unwrap();
         println!("yara-x: {}", datasize(out.len()));
 
-        println!("");
+        println!();
     }
 }
 
+#[cfg(feature = "serialize")]
 fn datasize(v: usize) -> String {
     if v > 1024 * 1024 {
         format!("{:.2}MB", (v as f64) / 1024. / 1024.)
