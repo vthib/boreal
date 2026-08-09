ed9940dfb330cd98bee0f0b0f076ea228569b416
Vincent Thiberville <vthib@pm.me>
2026-08-10 01:32:25 +0200

    feat: don't call Instant::now if statistics not enabled
    If the profiling feature is enabled, starting timers
    would be created even if no statistics were asked for
    and those timers would be unused. This isn't great as
    those Instant::now() calls are not free.
    
    This MR thus changes the behavior to only call Instant::now()
    if statistics are enabled.
    
    Note that this MR changes nothing if profiling feature is
    not enabled, in which case all this code is not included.

--- boreal/src/scanner/ac_scan.rs
+++ boreal/src/scanner/ac_scan.rs
@@ -173,7 +173,10 @@ impl AcScan {
 
         if !self.non_handled_var_indexes.is_empty() {
             #[cfg(feature = "profiling")]
-            let start = std::time::Instant::now();
+            let start = scan_data
+                .statistics
+                .as_ref()
+                .map(|_| std::time::Instant::now());
 
             // For every "raw" matcher, scan the memory for this matcher.
             for matcher_index in &self.non_handled_var_indexes {
@@ -184,7 +187,9 @@ impl AcScan {
 
             #[cfg(feature = "profiling")]
             if let Some(stats) = scan_data.statistics.as_mut() {
-                stats.raw_regexes_eval_duration += start.elapsed();
+                if let Some(start) = start {
+                    stats.raw_regexes_eval_duration += start.elapsed();
+                }
             }
         }
 
@@ -212,7 +217,10 @@ impl AcScan {
                 stats.nb_ac_matches += 1;
             }
             #[cfg(feature = "profiling")]
-            let start_instant = std::time::Instant::now();
+            let start_instant = scan_data
+                .statistics
+                .as_ref()
+                .map(|_| std::time::Instant::now());
 
             // Upscale to the original literal shape before feeding it to the matcher verification
             // function.
@@ -260,7 +268,9 @@ impl AcScan {
             #[cfg(feature = "profiling")]
             {
                 if let Some(stats) = scan_data.statistics.as_mut() {
-                    stats.ac_confirm_duration += start_instant.elapsed();
+                    if let Some(start) = start_instant {
+                        stats.ac_confirm_duration += start.elapsed();
+                    }
                 }
             }
 
